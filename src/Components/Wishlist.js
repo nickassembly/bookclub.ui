73b99f82236d0851ec1a16e921618c011eecb0e4
Nick Guerra <nickg@maliciousgames.com>
2020-04-21 20:25:54 -0500

    added get method to wishlist page

--- src/Components/Wishlist.js
+++ src/Components/Wishlist.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import Navbar from './Navbar';
 import {Table} from 'react-bootstrap';
+import Axios from 'axios';
+import {getJwt} from '../Helpers/Jwt';
 
 export default class Wishlist extends Component {
   constructor(props) {
     super(props);
-    this.state = {books: []};
+    this.state = {
+      books: [{}],
+    };
   }
 
   componentDidMount() {
@@ -13,13 +17,19 @@ export default class Wishlist extends Component {
   }
 
   refreshList() {
-    this.setState({
-      books: [
-        {Isbn: '0072223434', Author: 'King', Title: 'The Stand'},
-        {Isbn: '0514333434', Author: 'Tolkien', Title: 'The Hobbit'},
-      ],
-    });
+    const jwt = getJwt();
+    Axios.get(
+      'https://cors-anywhere.herokuapp.com/https://bookclubapi.azurewebsites.net/api/v1/books',
+      {headers: {Authorization: `bearer ${jwt}`}}
+    ).then((res) =>
+      this.setState({
+        books: res.data,
+      })
+    );
   }
+
+  // Need to figure out how to iterate through isbn, author, title and set key = id
+  // these fields are on the api but when I try to set them in the constructor they are coming back undefined
 
   render() {
     const {books} = this.state;
@@ -36,10 +46,10 @@ export default class Wishlist extends Component {
           </thead>
           <tbody>
             {books.map((books) => (
-              <tr key={books.bookId}>
-                <td>{books.Isbn}</td>
-                <td>{books.Author}</td>
-                <td>{books.Title}</td>
+              <tr>
+                <td>{books.data}</td>
+                <td>{books.data}</td>
+                <td>{books.data}</td>
               </tr>
             ))}
           </tbody>

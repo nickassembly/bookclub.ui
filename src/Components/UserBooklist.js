5a7f8ea8e296b3f4502eae31815b5c4c92ecbcde
Nick Guerra <nickg@maliciousgames.com>
2020-05-08 05:36:44 -0500

    added book structure

--- src/Components/UserBooklist.js
+++ src/Components/UserBooklist.js
@@ -6,26 +6,40 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import UserRow from './UserRow';
+import Axios from 'axios';
 import styles from './UserRow.css';
+import {getJwt} from '../Helpers/Jwt';
 
 type Props = {
-  books: [],
+  books: [book],
 };
 
 class UserBooklist extends Component {
+  componentDidMount() {
+    Axios.get('https://bookclubapi.azurewebsites.net/api/v1/books', {
+      // headers: {Authorization: `bearer ${jwt}`},
+    }).then((res) =>
+      this.setState({
+        books: res.data,
+      })
+    );
+  }
+
   render() {
     let bookBody = '';
     let bookRows = '';
     let bookHeading = '';
+
     bookHeading = (
       <TableRow>
-        <TableCell>Test</TableCell>
-        <TableCell>Test</TableCell>
-        <TableCell>Test</TableCell>
+        <TableCell>Isbn</TableCell>
+        <TableCell>Author</TableCell>
+        <TableCell>Title</TableCell>
       </TableRow>
     );
 
     // bookRows = this.props.books.map((book) => <UserRow key={book.id} {...book} />);
+    bookRows = <UserRow></UserRow>;
 
     bookBody = (
       <Table>

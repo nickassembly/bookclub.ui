a2632f611e3a5657e6c821747bcd1dfdf27f9de0
Nick Guerra <nickg@maliciousgames.com>
2020-06-03 06:57:40 -0500

    changed url in api.js to add slash before id

--- src/actions/api.js
+++ src/actions/api.js
@@ -6,9 +6,9 @@ export default {
   book(url = baseUrl + 'books') {
     return {
       getAll: () => axios.get(url),
-      getById: (id) => axios.get(url + id),
+      getById: (id) => axios.get(url + '/' + id),
       create: (newRecord) => axios.post(url, newRecord),
-      update: (id, updateRecord) => axios.put(url + id, updateRecord),
+      update: (id, updateRecord) => axios.put(url + '/' + id, updateRecord),
       deleteById: (id) => axios.delete(url + id),
     };
   },

ba6af815e04138395b25f1e2430e241fc5d8671f
Nick Guerra <nickg@maliciousgames.com>
2020-07-11 18:54:09 -0500

    removed unused libraries

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
-import {Provider} from 'react-redux';
 import UserBooklist from './Components/UserBooklist';
-import {ToastProvider} from 'react-toast-notifications';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-          <div className='container'>
-            <UserBooklist />
-          </div>
+    <div className='container'>
+      <UserBooklist />
+    </div>
   );
 }
 

821d1c189e778d99eb2cfc9d88b17f42067f27cd
Nick Guerra <nickg@maliciousgames.com>
2020-10-07 04:57:36 -0500

    changed spelling on App.css to fix issue on loading

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import '../App.css';
+import '../app.css';
 import {Button, Form, FormGroup, Label, Input} from 'reactstrap';
-import Cookies from 'js-cookie'
-import { Redirect } from "react-router-dom";
+import Cookies from 'js-cookie';
+import {Redirect} from 'react-router-dom';
 
 class Login extends Component {
   constructor(props) {
@@ -13,12 +13,12 @@ class Login extends Component {
       username: '',
       password: '',
       userId: Cookies.get('userId'),
-      token: Cookies.get('token') || "",
-      redirect: false
+      token: Cookies.get('token') || '',
+      redirect: false,
     };
     this.change = this.change.bind(this);
     this.attemptLogin = this.attemptLogin.bind(this);
-    if (this.state.token !== "")  {
+    if (this.state.token !== '') {
       console.log(`Token present ${this.state.token}`);
       this.state.redirect = true;
     }
@@ -34,32 +34,35 @@ class Login extends Component {
     const url = 'https://bookclubapi.azurewebsites.net/Users';
     const userInfo = {
       username: this.state.username,
-      password: this.state.password
+      password: this.state.password,
     };
-    axios.post(`${url}/authenticate`, userInfo)
-    .then((response) => {
-      Cookies.set('token',response.data.token, { expires: 365 });
-      Cookies.set('user',response.data.username, { expires: 365 });
-      Cookies.set('userId',response.data.id, { expires: 365 });
-      this.setState({
-        token: response.data.token,
-        cookieUser: response.data.username,
-        userId: response.data.id,
-        redirect: true
+    axios
+      .post(`${url}/authenticate`, userInfo)
+      .then((response) => {
+        Cookies.set('token', response.data.token, {expires: 365});
+        Cookies.set('user', response.data.username, {expires: 365});
+        Cookies.set('userId', response.data.id, {expires: 365});
+        this.setState({
+          token: response.data.token,
+          cookieUser: response.data.username,
+          userId: response.data.id,
+          redirect: true,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
       });
-    })
-    .catch(err => { console.log(err) })
-  };
+  }
 
   render() {
-    const { redirect } = this.state;
+    const {redirect} = this.state;
 
     if (redirect) {
-       return <Redirect to='/userbooklist'/>;
-     }
+      return <Redirect to='/userbooklist' />;
+    }
     return (
       <div>
-        <Form className='login-form' >
+        <Form className='login-form'>
           <h1 className='text-center'>
             <span className='font-weight-bold'>Book Club</span>
           </h1>
